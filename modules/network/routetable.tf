f74077fd2129825be78b9bd616a894188e8f3f35
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2023-10-18 17:04:47 +0530

    added pvt route table

--- modules/network/routetable.tf
+++ modules/network/routetable.tf
@@ -12,3 +12,13 @@ resource "aws_route_table_association" "demoProject-pubsubnet-1" {
   subnet_id      = aws_subnet.demoProjectPublicSubnet-1.id
   route_table_id = aws_route_table.demoProjectPublicRT.id
 }
+resource "aws_route_table" "demoProjectPrivateRT" {
+ vpc_id = aws_vpc.demoProject.id
+ tags = {
+   Name = "${var.vpc_name}-Pvt-RT"
+ }
+}
+resource "aws_route_table_association" "demoProject-pvtsubnet-1" {
+  subnet_id      = aws_subnet.demoProjectPrivateSubnet-1.id
+  route_table_id = aws_route_table.demoProjectPrivateRT.id
+}
