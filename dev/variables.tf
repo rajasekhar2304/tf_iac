de4753a656f030a05ac7ee8fda0a8527db4db622
root <root@ip-10-20-6-96.ec2.internal>
2023-10-18 09:36:26 +0000

    terraform fmt

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,6 +1,6 @@
-variable vpc_cidr {}
-variable vpc_name {}
-variable owner {}
-variable environment {}
-variable public_subnet1_cidr {}
-variable private_subnet1_cidr {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "owner" {}
+variable "environment" {}
+variable "public_subnet1_cidr" {}
+variable "private_subnet1_cidr" {}
