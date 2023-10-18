f8e64007bdfdfdd2c14491dee459d9d6cdeeb6f8
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2023-10-18 15:04:48 +0530

    modified backend.tf

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
  backend "s3" {
-   bucket         = "demoprojecttfstatefile"
+   bucket         = "b27-tfstatefile"
    key            = "terraform.tfstate"
    region         = "us-east-1"
-   dynamodb_table = "demoproject-terraform-locking"
+   dynamodb_table = "devopsb27-terraform-locking"
    encrypt        = true
  }
 }
