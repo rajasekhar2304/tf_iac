ccbb3f22b95ad15e9956baf3b936092f5cf2a77c
Raja Sekhar Gurugubelli <rgurugubelli@cog.int>
2023-10-19 12:51:08 +0530

    modified pubRouteTable.tf file

--- pubRouteTable.tf
+++ pubRouteTable.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "demoProjectPubRT" {
   vpc_id = aws_vpc.demoProject.id
   route = {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.id
+    gateway_id = aws_internet_gateway.demoProject.id
   }
   tags = {
     Name = "${var.vpc_name}-Pub-RT"
