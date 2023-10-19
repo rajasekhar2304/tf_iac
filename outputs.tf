4a0bab2936a363003da51e3c6a28f446982ba11d
root <root@ip-10-20-4-238.ec2.internal>
2023-10-19 09:12:36 +0000

    terraform fmt command is applied

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,48 @@
 output "vpc_id" {
-    value = aws_vpc.demoProject.id
-    sensitive = false  
+  value     = aws_vpc.demoProject.id
+  sensitive = false
 }
 output "vpc_arn" {
-    value = aws_vpc.demoProject.arn
-    sensitive = false  
+  value     = aws_vpc.demoProject.arn
+  sensitive = false
 }
 output "igw_id" {
-    value = aws_internet_gateway.demoProject.id
-    sensitive = false  
+  value     = aws_internet_gateway.demoProject.id
+  sensitive = false
 }
 output "pub_subnet_1_id" {
-    value = aws_subnet.demoProjectPubSubnet-1.id
-    sensitive = false
+  value     = aws_subnet.demoProjectPubSubnet-1.id
+  sensitive = false
 }
 output "pub_subnet_2_id" {
-    value = aws_subnet.demoProjectPubSubnet-2.id
-    sensitive = false 
+  value     = aws_subnet.demoProjectPubSubnet-2.id
+  sensitive = false
 }
 output "pub_subnet_3_id" {
-    value = aws_subnet.demoProjectPubSubnet-3.id
-    sensitive = false  
+  value     = aws_subnet.demoProjectPubSubnet-3.id
+  sensitive = false
 }
 output "pvt_subnet_1_id" {
-    value = aws_subnet.demoProjectPvtSubnet-1.id
-    sensitive = false  
+  value     = aws_subnet.demoProjectPvtSubnet-1.id
+  sensitive = false
 }
 output "pvt_subnet_2_id" {
-    value = aws_subnet.demoProjectPvtSubnet-2.id
-    sensitive = false
+  value     = aws_subnet.demoProjectPvtSubnet-2.id
+  sensitive = false
 }
 output "pvt_subnet_3_id" {
-    value = aws_subnet.demoProjectPvtSubnet-3.id
-    sensitive = false  
+  value     = aws_subnet.demoProjectPvtSubnet-3.id
+  sensitive = false
 }
 output "pub_rt_id" {
-    value = aws_route_table.demoProjectPubRT.id
-    sensitive = false  
+  value     = aws_route_table.demoProjectPubRT.id
+  sensitive = false
 }
 output "pvt_rt_id" {
-    value = aws_route_table.demoProjectPvtRT.id
-    sensitive = false  
+  value     = aws_route_table.demoProjectPvtRT.id
+  sensitive = false
 }
 output "sg_id" {
-    value = aws_security_group.demoProjectSG.id
-    sensitive = false  
+  value     = aws_security_group.demoProjectSG.id
+  sensitive = false
 }
